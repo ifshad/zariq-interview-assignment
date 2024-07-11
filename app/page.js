16e1c5c19d0a76f646713a748d5021d0762ca759
ifshad <shoronshark@gmail.com>
2024-07-12 02:30:56 +0600

    navbar completed

--- app/page.js
+++ app/page.js
@@ -1,9 +1,4 @@
-import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      hello
-    </main>
-  );
+  return <div>hello</div>;
 }
