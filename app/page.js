ecd2f140886019d9467b1f0e805e184cc420149c
ifshad <shoronshark@gmail.com>
2024-07-12 12:11:25 +0600

    footer section completed

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import ExamReminder from "@/components/ExamReminder/page";
 export default function Home() {
   return (
     <div className="container">
+      <h1>hello</h1>
       <ExamReminder />
     </div>
   );
---
+++ constants/navigationData.js
@@ -0,0 +1,157 @@
+export const navListItems = [
+  {
+    id: 1,
+    title: "Home",
+    link: "/",
+  },
+  {
+    id: 2,
+    title: "About us",
+    link: "/about-us",
+    subMenu: [
+      {
+        id: 21,
+        title: "Vision, Mission and Values",
+        link: "/about-us/vision-mission-values",
+      },
+      {
+        id: 22,
+        title: "Why choose us",
+        link: "/about-us/why-choose-us",
+      },
+      {
+        id: 23,
+        title: "Principal Message",
+        link: "/about-us/principal-message",
+      },
+      {
+        id: 24,
+        title: "Our Team",
+        link: "/about-us/our-team",
+      },
+      {
+        id: 25,
+        title: "Curriculum",
+        link: "/about-us/curriculum",
+      },
+      {
+        id: 26,
+        title: "History",
+        link: "/about-us/history",
+      },
+      {
+        id: 27,
+        title: "Campus",
+        link: "/about-us/campus",
+      },
+      {
+        id: 28,
+        title: "Upcoming Events",
+        link: "/about-us/upcoming-events",
+      },
+      {
+        id: 29,
+        title: "Gallery",
+        link: "/about-us/gallery",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Admission",
+    link: "/admission",
+    subMenu: [
+      {
+        id: 31,
+        title: "A brief admission Procedure",
+        link: "/admission/procedure",
+      },
+      {
+        id: 32,
+        title: "Scholarship",
+        link: "/admission/scholarship",
+      },
+      {
+        id: 33,
+        title: "School fees",
+        link: "/admission/school-fees",
+      },
+      {
+        id: 34,
+        title: "Withdrawal Policy",
+        link: "/admission/withdrawal-policy",
+      },
+      {
+        id: 35,
+        title: "FAQ on Admissions",
+        link: "/admission/faq",
+      },
+      {
+        id: 36,
+        title: "How to Apply",
+        link: "/admission/how-to-apply",
+      },
+      {
+        id: 37,
+        title: "Apply Online",
+        link: "/admission/apply-online",
+      },
+      {
+        id: 38,
+        title: "Online Payment",
+        link: "/admission/online-payment",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Contact us",
+    link: "/contact-us",
+  },
+  {
+    id: 5,
+    title: "Achievements",
+    link: "/achievements",
+  },
+  {
+    id: 6,
+    title: "Key Information",
+    link: "/key-information",
+  },
+  {
+    id: 7,
+    title: "Policies",
+    link: "/policies",
+    refUrl: "DPS STS School",
+  },
+  {
+    id: 8,
+    title: "Facilities",
+    link: "/facilities",
+  },
+];
+
+export const drawerItemsList = [
+  {
+    branch: "Satarkul Branch",
+    contactNumber: "+8809677444777",
+    email: "info@glenrich.edu.bd",
+    address:
+      "Plot No: E-2 & E-3, Sunvalley Sharoni, Block: A, Sunvalley Abashan Swadesh Properties Ltd., Satarkul, Badda, Dhaka-1212",
+    visitCampus: "Visit Satarkul Campus",
+  },
+  {
+    branch: "Uttara Branch (Junior Campus)",
+    contactNumber: "(+88) 0248961087",
+    email: "info@uttara.glenrich.edu.bd",
+    address: "Plot 4, Road 13, Sector 6, Uttara Model Town, Dhaka-1230",
+    visitCampus: "Visit Uttara Campus",
+  },
+  {
+    branch: "Uttara Branch (Senior Campus)",
+    contactNumber: "(+88) 09614087141",
+    email: "info@uttara.glenrich.edu.bd",
+    address: "Plot ED-01, Road 1, Sector 15, Uttara Model Town, Dhaka-1230",
+    visitCampus: "Visit Uttara Campus",
+  },
+];
