18a3de3e4ce36cc126d743cd556019c95e61ae51
ifshad <shoronshark@gmail.com>
2024-07-13 14:44:10 +0600

    why choose stylescompleted

--- components/WhyChooseUs/page.jsx
+++ components/WhyChooseUs/page.jsx
@@ -26,30 +26,46 @@ export default function WhyChooseUs() {
   //   console.log(selectedOption);
   return (
     <div className="container md:p-12">
-      <div className="bg-slate-300">
-        {buttons.map((button) => (
-          <button
-            onClick={() => setSelectedOption(button.id)}
-            className={`p-4 hover:bg-slate-50 ${
-              button.id === selectedOption && "bg-slate-50"
-            }`}
-            key={button.id}
-          >
-            {button.title}
-          </button>
-        ))}
-      </div>
-      <div>
-        {whyChooseDetails.map((item) => {
-          if (item.id === selectedOption) {
-            return (
-              <div key={item.id} className={`p-4 bg-slate-50`}>
-                <h1>{item.title}</h1>
-                <p>{item.description}</p>
-              </div>
-            );
-          }
-        })}
+      <div className="grid grid-cols-1 md:grid-cols-3 md:gap-0">
+        {/* Images */}
+        <div className="md:col-span-1 bg-slate-50">
+          {whyChooseDetails.map((item) => {
+            if (item.id === selectedOption) {
+              return <p key={item.id}>{item.imageUrl}</p>;
+            }
+          })}
+        </div>
+        {/* Description */}
+        <div className="md:col-span-2">
+          {/* Buttons */}
+          <div className="bg-slate-300">
+            {buttons.map((button) => (
+              <button
+                onClick={() => setSelectedOption(button.id)}
+                className={`p-4 w-1/4 text-slate-700 hover:bg-slate-50 ${
+                  button.id === selectedOption &&
+                  "bg-slate-50 text-slate-900 font-semibold"
+                }`}
+                key={button.id}
+              >
+                {button.title}
+              </button>
+            ))}
+          </div>
+          {/* Descriptions */}
+          <div className="">
+            {whyChooseDetails.map((item) => {
+              if (item.id === selectedOption) {
+                return (
+                  <div key={item.id} className={`p-4 bg-slate-50`}>
+                    <h1 className="card-title mt-5 mb-3">{item.title}</h1>
+                    <p className="mb-5">{item.description}</p>
+                  </div>
+                );
+              }
+            })}
+          </div>
+        </div>
       </div>
     </div>
   );
